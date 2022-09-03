5c3243baa97cbabc4d59218f04b4883e130551dd
tkachenkoevgeniy <108264970+tkachenkoevgeniy@users.noreply.github.com>
2022-09-03 17:28:18 +0300

    Delete ICompany.java

--- Hello - oop/src/telran/people/ICompany.java
+++
@@ -1,9 +0,0 @@
-package telran.people;
-
-public interface ICompany {
-boolean addEmployee(Employee empl);
-Employee removeEmployee(long id);
-Employee getEmployee(long id);
-long computeSalaryBudget();
-Employee[] getAllEmployees();
-}
